18fd7082be840a4636dc07cb78ddbb247ff81e97
miacarmen <maitreyacarmen@gmail.com>
2022-05-08 16:35:43 -0700

    added blogpost model

---
+++ models/BlogPost.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "blogpost",
+        key: "id",
+      },
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    post_body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    date_created: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blogpost",
+  }
+);
+
+module.exports = BlogPost;
