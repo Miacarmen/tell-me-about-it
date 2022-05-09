4ece7037436cc8910b8c99bd7f0a56acbef44e0c
miacarmen <maitreyacarmen@gmail.com>
2022-05-08 21:51:08 -0700

    added login and signup form handlers

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,23 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
-    
-}
+    // query selector for username
+    const username = document.querySelector('#username-login').value.trim();
+    // query selector for password
+    const password = document.querySelector('#password-login').value.trim();
+
+    if(username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }), 
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if(response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in');
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
