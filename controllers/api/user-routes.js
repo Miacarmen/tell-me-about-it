eafe141e004fdb98792bf15e944a4b78959962c8
miacarmen <maitreyacarmen@gmail.com>
2022-05-09 14:20:18 -0700

    added homepage and user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,19 +1,74 @@
 const router = require('express').Router();
 
-const { user } = require('../../models');
+const { user, User } = require('../../models');
 
 
-// POST signup
-// prompt to sign up
-// then user credentials are saved
+// POST Signup
+router.post('/', async (req, res) => {
+    try {
+        // Create new user
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+        // automatically logs in the user once they are created
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.username = newUser.username;
+            // user is logged in
+            req.session.logged_in = true;
+            res.json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// POST login
-// prompt to sign in
+// POST Login
+router.post('/login', async (req, res) => {
+    try {
+        // check if user exsists in db
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
+        // if user does not exist, throw err
+        if(!userData) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+        // validate password
+        const validPassword = await userData.checkPassword(req.body.password);
+        // if password is not valid
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+        // if user is valid, save login session to the db
+        req.session.save(() => {
+            req.session.user_id = userData.isSoftDeleted;
+            req.session.logged_in = true;
 
-// when they revist a site and click login
-// then prompt to enter username and password
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 
 // POST logout
-// destroy session
+router.post('/logout', (req, res) => {
+    // if user is logged in
+    if(req.session.logged_in) {
+        // destroy session
+        req.session.destroy(() => {
+            res.status(200).end();
+        });
+    } else {
+        res.status(400).end();
+    }
+});
 
 module.exports = router;
--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,21 +1,3 @@
-const router = require('express').Router();
-
-const { User, BlogPost, Comment } = require('../models');
-const withAuth = require('../utils/auth');
-
-// GET all blog posts and join with user data
-
-// GET one post by id
-
-
-// GET login
-
-
-
-// GET signup
-
-
-module.exports = router;
 // When a new user visits site
 // they are presented with the homepage
 // includes exsisting blog posts if any have been posted
@@ -30,3 +12,76 @@ module.exports = router;
 // then prompt to create a useername and password
 // when sign-up button is clicked
 // then the users credentials are saved to the database and they are now logged in
+
+const router = require("express").Router();
+
+const { User, BlogPost, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+// GET all blog posts and join with user data
+router.get("/", async (req, res) => {
+  try {
+    const postData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    // serialize data so handlebars template can read it
+    const posts = postData.map((post) =>
+      post.get({
+        plain: true,
+      })
+    );
+    // pass serialized data and session flag into template
+    res.render("all-posts", { posts, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET one post by id
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await BlogPost.findOne({
+            where: {id: req.params.id},
+            include: [User, {
+                // ****** WHAT
+                model: Comment,
+                include: User,
+            },
+        ],
+        });
+        if(postData) {
+            const post = postData.get({ plain: true });
+            // pass serialized data and session flag into template
+            res.render('single-post', { post, logged_in: req.session.loggedIn });
+        } else {
+            res.status(400).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET login
+router.get('/login', (req, res) => {
+    // If a session exsists and user is already logged in, redirect the request to the dashboard
+    if(req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
+
+// GET signup
+router.get('/signup', (req, res) => {
+    if(!req.session.logged_in) {
+        res.redirect('/signup');
+    }
+    res.render('signup');
+})
+
+module.exports = router;
+
